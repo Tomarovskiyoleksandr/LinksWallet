cbf08de318f3c85da3b49a7a1769de06877a20d2
Tomarovskiyoleksandr <tomarovskijoleksandr@gmail.com>
2026-09-01 17:39:06 +0200

    Fix PostgreSQL SSL configuration

--- server/db/db.js
+++ server/db/db.js
@@ -3,11 +3,9 @@ const { Pool } = require("pg");
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 
-    ssl: process.env.NODE_ENV === "production"
-        ? false
-        : {
-            rejectUnauthorized: false
-        }
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
